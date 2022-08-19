3013255fde5e94a9df283aea14e8b03c645bd2dc
est8r <estermessias6@gmail.com>
2022-08-18 23:46:44 -0300

    Transformando a requisição em json

--- sedex-sprint/src/components/BuscaApi/BuscaApi.jsx
+++ sedex-sprint/src/components/BuscaApi/BuscaApi.jsx
@@ -3,6 +3,16 @@ import { useState } from 'react';
 import Input from '../Input/Input.jsx';
 
 const BuscaApi = () => {
+  const [valueInput, setValueInput] = useState();
+  const [cep, setCEP] = useState();
+  const handleSetValue = (e) => {
+    setCEP(e.target.value);
+  };
+  const handleRequisicao = async () => {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const json = await response.json();
+    setValueInput(json);
+  };
   return (
     <div>
       <Input />
