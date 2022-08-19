1febf984495c92dfb269653f26fea0ba3e27c422
est8r <estermessias6@gmail.com>
2022-08-18 23:44:55 -0300

    Importando componente de input

--- sedex-sprint/src/components/BuscaApi/BuscaApi.jsx
+++ sedex-sprint/src/components/BuscaApi/BuscaApi.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
+import Input from '../Input/Input.jsx';
 
 const BuscaApi = () => {
   return (
     <div>
-      <input />
+      <Input />
 
       <button>Buscar</button>
       <p></p>
