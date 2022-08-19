4d61d7da31bd7058951e5c4d8397161a4f68d806
est8r <estermessias6@gmail.com>
2022-08-18 23:43:29 -0300

    Criação do componente BucaApi para requisição

---
+++ sedex-sprint/src/components/BuscaApi/BuscaApi.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState } from 'react';
+
+const BuscaApi = () => {
+  return (
+    <div>
+      <input />
+
+      <button>Buscar</button>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  );
+};
+
+export default BuscaApi;
