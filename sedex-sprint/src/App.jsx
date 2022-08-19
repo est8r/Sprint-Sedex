cc7857ce9e6f0a35c32cdc5d76e2a24febf55168
est8r <estermessias6@gmail.com>
2022-08-18 23:52:13 -0300

    Importando componente BuscaApi

--- sedex-sprint/src/App.jsx
+++ sedex-sprint/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-
+import BuscaApi from './components/BuscaApi/BuscaApi';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BuscaApi />
+    </div>
+  );
 }
 
 export default App;
