5ebfc80b3bf04f818849b09e7684cb6835e39e41
Erika Rodrigues <erikarodrig.contato@gmail.com>
2022-08-19 18:07:25 -0300

    criação do componente card
    Signed-off-by: Erika Rodrigues <erikarodrig.contato@gmail.com>

---
+++ sedex-sprint/src/components/card/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = ({cep,logradouro,bairro,localidade, uf}) => {/*destructing */
+  return (
+    <div>
+        <h2>cep:{cep}</h2>
+        <h2>uf:{uf}</h2>
+        <h2>rua:{logradouro}</h2>
+        <h3>bairro:{bairro}</h3>
+        <h3>localidade:{localidade}</h3>
+    </div>
+  )
+}
+
+export default Card
