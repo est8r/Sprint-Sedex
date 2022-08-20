39988d157f046f50150381d464761b517bd75954
est8r <estermessias6@gmail.com>
2022-08-19 21:19:23 -0300

    Importando css e adicionando tags de separação

--- sedex-sprint/src/components/BuscaApi/BuscaApi.jsx
+++ sedex-sprint/src/components/BuscaApi/BuscaApi.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import S from '../BuscaApi/BuscaApi.module.css';
 import Button from '../Button/Button';
 import Card from '../Card/Card';
 import Label from '../Label/Label';
@@ -21,38 +22,49 @@ const BuscaApi = () => {
   };
 
   return (
-    <div>
-      <Label texto="Digite um CEP: " />
-      <input
-        defaultValue={cep}
-        type="text"
-        placeholder="Seu CEP"
-        onChange={handleSetValue}
-      />
-
-      <Button onClick={handleRequisicao} />
-      {card ? (
-        <Card
-          cep={card.cep}
-          uf={card.uf}
-          rua={card.logradouro}
-          cidade={card.localidade}
-          bairro={card.bairro}
-        />
-      ) : (
-        ''
-      )}
-      {historico
-        ? historico.map((item) => (
+    <div className={S.div}>
+      <section className={S.section}>
+        <div className={S.send}>
+          <Label texto="Digite um CEP: " />
+          <input
+            className={S.input}
+            defaultValue={cep}
+            type="text"
+            placeholder="Seu CEP"
+            onChange={handleSetValue}
+          />
+          <Button className={S.btn} onClick={handleRequisicao} />
+        </div>
+      </section>
+      <section className={S.output}>
+        <div className={S.card}>
+          {card ? (
             <Card
-              cep={item.cep}
-              uf={item.uf}
-              rua={item.logradouro}
-              cidade={item.localidade}
-              bairro={item.bairro}
+              cep={card.cep}
+              uf={card.uf}
+              logradouro={card.logradouro}
+              localidade={card.localidade}
+              bairro={card.bairro}
             />
-          ))
-        : ''}
+          ) : (
+            ''
+          )}
+        </div>
+
+        <div className={S.history}>
+          {historico
+            ? historico.map((item) => (
+                <Card
+                  cep={item.cep}
+                  uf={item.uf}
+                  logradouro={item.logradouro}
+                  localidade={item.localidade}
+                  bairro={item.bairro}
+                />
+              ))
+            : ''}
+        </div>
+      </section>{' '}
     </div>
   );
 };
