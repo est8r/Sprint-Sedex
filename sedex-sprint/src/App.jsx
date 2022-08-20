775baf1a2369d335750f1bc647593916eafc4da1
est8r <estermessias6@gmail.com>
2022-08-19 21:13:40 -0300

    Importando o componente Header

--- sedex-sprint/src/App.jsx
+++ sedex-sprint/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import Header from './components/Header/Header.jsx';
 import BuscaApi from './components/BuscaApi/BuscaApi';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BuscaApi />
     </div>
   );
